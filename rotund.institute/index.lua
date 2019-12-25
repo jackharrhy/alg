44aff3cab1df3ce2f7261e4dbf958907d0dbab61
Jack Arthur Harrhy <me@jackharrhy.com>
2019-12-25 18:49:57 -0330

    browsers are fun and do not like mkv files

--- rotund.institute/index.lua
+++ rotund.institute/index.lua
@@ -4,7 +4,7 @@ local key = data:getstring("key")
 local urldata = urldata()
 
 if urldata.key == key and urldata.series and urldata.episode then
-  local file = "files/" .. urldata.series .. "/" .. urldata.episode .. ".mkv"
+  local file = "files/" .. urldata.series .. "/" .. urldata.episode .. ".mp4"
   log("serving: "..file)
   content("video/mp4")
   serve(file)
