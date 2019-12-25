5605b22db32b0b902e47954c1a7ee385400037a9
Jack Arthur Harrhy <me@jackharrhy.com>
2019-12-25 18:37:22 -0330

    logs

--- rotund.institute/index.lua
+++ rotund.institute/index.lua
@@ -5,9 +5,11 @@ local urldata = urldata()
 
 if urldata.key == key and urldata.series and urldata.episode then
   local file = "files/" .. urldata.series .. "/" .. urldata.episode .. ".mkv"
+  log("serving: "..file)
   content("video/mp4")
   serve(file)
 else
+  log("serving homepage")
   content("text/html")
   serve("index.html")
 end
--- rotund.institute/store.lua
+++ rotund.institute/store.lua
@@ -1,9 +1,12 @@
 local data = JFile("data.json")
 local key = data:getstring("key")
+local users_key = urldata().key
 
-if urldata().key == key then
+if users_key == key then
   content("application/json")
+  log("serving data")
   print(tostring(data))
 else
+  log("incorrect key: "..users_key)
   status(403)
 end
